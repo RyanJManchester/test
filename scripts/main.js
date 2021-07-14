b5617aae35d104ec70585f4d8a3242eb4794946a
RyanJManchester <ryanjamesmanchester@gmail.com>
2021-07-14 16:33:41 +1200

    reset for javascript learning

--- scripts/main.js
+++
@@ -1,6 +0,0 @@
-const myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello World!';
-
-/* Studying up on basic core concepts now; strings,
-array's, objects, conditionals and functions
-*/
