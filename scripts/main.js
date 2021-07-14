80a14693f9cd27586ee20eb8ac89f8dfc5ff1a34
RyanJManchester <ryanjamesmanchester@gmail.com>
2021-07-14 17:27:37 +1200

    ^

--- scripts/main.js
+++ scripts/main.js
@@ -5,6 +5,28 @@ myImage.onclick = function() {
     if(mySrc === 'images/firefox-logo.png') {
         myImage.setAttribute('src','images/grey-firefox-logo.png');
     } else {
-        myImage.setAttribute('src', 'images/firefoc-logo.png');
+        myImage.setAttribute('src', 'images/firefox-logo.png');
     }
+}
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+    let myName = prompt('Please enter your name');
+    if(!myName) {
+        setUserName();
+    } else {
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Mozilla is cool, ' + myName;
+    }
+}
+
+if(!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
+myButton.onclick = function() {
+    setUserName();
 }
